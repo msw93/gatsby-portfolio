75b5df8490b1a18422474ba163aca349641e108a
mikewiner <winer.mike@gmail.com>
2020-07-08 16:51:23 -0400

    graphql querying added

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,13 +2,15 @@ import React from "react"
 import Header from "../components/header.js"
 import Textbox from "../components/textbox.js";
 import Fade from 'react-reveal/Fade';
+import Img from 'gatsby-image';
 
 export default function Portfolio() {
+
   return (
     <Textbox id="portfolio" style={{ color: `black` }}>
       <Fade duration={3000} delay={500} distance="30px">
-          <Header headerText="Portfolio" />
-        	<p className="">You can find my portfolio at Github</p>
+          <Header headerText="Projects" />
+        	<p className="">You can find more projects at Github</p>
         	<a href='https://github.com/mikewiner'>Click here.</a>
       </Fade>
     </Textbox>
