d2d3060aaf27f5466523dc233c408b913df05bea
mikewiner <winer.mike@gmail.com>
2020-08-25 01:39:52 -0400

    added styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,12 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
