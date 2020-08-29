7076d7655b521fbfc9b19440089a0be6236f1bb4
mikewiner <winer.mike@gmail.com>
2020-08-29 15:27:21 -0400

    resume update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,37 @@
-import React from "react";
+import React from "react"
 import { Helmet } from "react-helmet"
-import Navbar from "../components/Navbar.js";
-import Intro from "../components/Intro.js";
-import FooterSelfmade from "../components/FooterSelfmade.js";
-import About from '../components/About.js';
-import Contact from '../components/Contact.js';
-import Portfolio from '../components/Portfolio.js';
-import ClientOnly from '../components/ClientOnly'
+import Navbar from "../components/Navbar.js"
+import Intro from "../components/Intro.js"
+import FooterSelfmade from "../components/FooterSelfmade.js"
+import About from "../components/About.js"
+import Contact from "../components/Contact.js"
+import Portfolio from "../components/Portfolio.js"
+import ClientOnly from "../components/ClientOnly"
 
+const Home = ({ data }) => (
+  <div>
+    <Helmet>
+      <meta
+        charSet="utf-8"
+        name="viewport"
+        content="width=device-width, initial-scale=1 shrink-to-fit=no, viewport-fit=cover"
+      />
+      <html lang="en" />
+      <title>{data.site.siteMetadata.title}</title>
+      <link rel="canonical" href="https://mikewiner.ca" />
+      <meta name="description" content={data.site.siteMetadata.description} />
+    </Helmet>
 
-const Home = ({data}) => ( 
-  	 
-    
-    <div>    
-      <Helmet>
-        <meta charSet="utf-8" name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no, viewport-fit=cover" />
-        <html lang='en'/>
-        <title>{data.site.siteMetadata.title}</title>
-        <link rel="canonical" href="https://mikewiner.ca" />
-        <meta name="description" content={data.site.siteMetadata.description} />
-      </Helmet>
-
-      <ClientOnly>
-        <Navbar />
-        <Intro />
-        <About />
-        <Portfolio />
-        <Contact />
-        <FooterSelfmade />
-      </ClientOnly>
-
-  	</div>
-);
+    <ClientOnly>
+      <Navbar />
+      <Intro />
+      <About />
+      <Portfolio />
+      <Contact />
+      <FooterSelfmade />
+    </ClientOnly>
+  </div>
+)
 
 export const query = graphql`
   query HomePageQuery {
@@ -44,4 +44,4 @@ export const query = graphql`
   }
 `
 
-export default Home;
+export default Home
