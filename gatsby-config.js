0ea22fa44808746ddbaa6f1747a37416cd1bea7e
mikewiner <winer.mike@gmail.com>
2020-09-09 17:19:14 -0400

    typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
+        name: "Portfolio Website",
+        short_name: "portfolio-site",
         start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
+        background_color: "#5ed8d8",
+        theme_color: "#5ed8d8",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
